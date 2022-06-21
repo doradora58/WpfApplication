307278b6fe8c2afe352c94a84dda2bec93008945
doradora58 <koudai58@gmail.com>
2022-06-22 00:04:36 +0900

    プロジェクト ファイルを追加します。

---
+++ WpfApplication/Commands/OpenMessageDialogCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTaskApplication.Commands
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class OpenMessageDialogCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var name = parameter as string;
+
+            var message = "こんにちは、" + name + "さん";
+
+            MessageBox.Show(message);
+        }
+    }
+}
---
+++ WpfApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication.ViewModels
+{
+    using WpfTaskApplication.Commands;
+    using WpfApplication.Models;
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.OpenMessageDialogCommand = new OpenMessageDialogCommand();
+            this.UserModel = new UserModel();
+        }
+
+        public OpenMessageDialogCommand OpenMessageDialogCommand { get; set; }
+
+        public UserModel UserModel { get; set; }
+    }
+}
